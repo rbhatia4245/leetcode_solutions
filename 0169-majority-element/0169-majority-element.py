166cfc26fdd084c737cbe9fc686c933d75077f3a
Rishi Bhatia <rbhatia4245@gmail.com>
2023-06-26 01:05:59 -0400

    Time: 145 ms (39.05%), Space: 15 MB (64.18%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        m = defaultdict(int)
+        
+        for num in nums:
+            m[num] += 1
+        
+        n = n // 2
+        for key, value in m.items():
+            if value > n:
+                return key
+        
+        return 0
