209e4bf388e77780ac26c094c9e0bb784ea515ed
Rishi Bhatia <rbhatia4245@gmail.com>
2023-07-11 15:00:41 -0400

    Time: 1331 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+Select id,
+CASE WHEN id % 2 = 0 THEN LAG(student) OVER (ORDER BY id) 
+ELSE COALESCE(
+  LEAD(student) OVER (ORDER BY id), student) END as student
+FROM Seat
