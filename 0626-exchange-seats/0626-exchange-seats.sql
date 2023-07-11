d446480c060892b1e1448ec785dda43397ae8243
Rishi Bhatia <rbhatia4245@gmail.com>
2023-07-11 15:03:58 -0400

    Time: 736 ms (60.06%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
+# if id is odd select student name for the next id
+# if id is even select student name for the previous id
+# When number of student is odd last element will be null so in case of null keep the original student name.
 Select id,
 CASE WHEN id % 2 = 0 THEN LAG(student) OVER (ORDER BY id) 
 ELSE COALESCE(
   LEAD(student) OVER (ORDER BY id), student) END as student
-FROM Seat
+FROM Seat;
