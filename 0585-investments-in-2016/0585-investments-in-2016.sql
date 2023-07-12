19b64664a45032e6721dcebe19e0342874397609
Rishi Bhatia <rbhatia4245@gmail.com>
2023-07-12 14:28:49 -0400

    Time: 844 ms (96.82%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -11,8 +11,9 @@ WITH matching_2015_values AS (
 ),
 unique_locations AS (
   SELECT distinct *
-  FROM matching_2015_values
-  WHERE pid NOT IN
-  (SELECT m2v.pid FROM matching_2015_values m2v INNER JOIN Insurance i ON m2v.pid <> i.pid and m2v.lat = i.lat and m2v.lon = i.lon)
+  FROM matching_2015_values m2v
+  WHERE NOT EXISTS (
+    Select 1 FROM Insurance i where m2v.pid <> i.pid and m2v.lat = i.lat and m2v.lon = i.lon
+  )
 )
 SELECT ROUND(SUM(tiv_2016), 2) as tiv_2016 from unique_locations
